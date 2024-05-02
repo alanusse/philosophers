8404dd7b71370c1767aa4385739d4c4b9ce114e5
Agustín Lanusse <lanusseagustin@gmail.com>
2024-05-02 23:37:11 +0200

    a

--- philo/includes/philosophers.h
+++ philo/includes/philosophers.h
@@ -6,7 +6,7 @@
 /*   By: aglanuss <aglanuss@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/26 13:22:44 by aglanuss          #+#    #+#             */
-/*   Updated: 2024/04/29 18:42:22 by aglanuss         ###   ########.fr       */
+/*   Updated: 2024/05/01 13:12:39 by aglanuss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 # define PHILOSOPHERS_H
 # include <stdlib.h>
 # include <stdio.h>
+# include <pthread.h>
 
 # define USAGE_MESSAGE "Usage: ./philo <number_of_philosophers> <time_to_die> \
 <time_to_eat> <time_to_sleep> <number_of_times_each_philosopher_must_eat>\n\
@@ -23,6 +24,12 @@
 - time_to_sleep: number greater than 0 in milliseconds\n\
 - number_of_times_each_philosopher_must_eat: optional number\
 greater than -1"
+
+typedef struct s_philo
+{
+	pthread_t	*thread;
+	int				id;
+}							t_philo;
 
 int	is_valid_arguments(int argc, char **argv);
 
--- philo/src/main.c
+++ philo/src/main.c
@@ -6,18 +6,44 @@
 /*   By: aglanuss <aglanuss@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/26 13:07:55 by aglanuss          #+#    #+#             */
-/*   Updated: 2024/04/29 18:08:50 by aglanuss         ###   ########.fr       */
+/*   Updated: 2024/05/02 23:37:00 by aglanuss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/philosophers.h"
+#include "../includes/utils.h"
+
+int	init_philos(t_philo **philos, int num_of_philos)
+{
+	int	i;
+
+	philos = malloc(sizeof(t_philo *) * num_of_philos);
+	i = -1;
+	while (++i < num_of_philos)
+	{
+		philos[i] = malloc(sizeof(t_philo));
+		philos[i]->id = i + 1;
+		philos[i]->thread = malloc(sizeof(pthread_t *));
+	}
+	return (1);
+}
 
 int	main(int argc, char **argv)
 {
+	t_philo	**philos;
+
 	if (!is_valid_arguments(argc, argv))
 	{
 		printf("%s\n", USAGE_MESSAGE);
 		return (EXIT_FAILURE);
+	}
+	philos = NULL;
+	init_philos(philos, ft_atoi(argv[1]));
+
+	int i = -1;
+	while (++i < ft_atoi(argv[1]))
+	{
+		printf("philo id: %i\n", philos[i]->id);
 	}
 	return (0);
 }
