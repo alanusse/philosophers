571c7a3b1e28e02e2685929345ab64253b600e5e
Agustín Lanusse <lanusseagustin@gmail.com>
2024-05-06 14:35:03 +0200

    init variables

--- philo/src/main.c
+++ philo/src/main.c
@@ -6,12 +6,30 @@
 /*   By: aglanuss <aglanuss@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/26 13:07:55 by aglanuss          #+#    #+#             */
-/*   Updated: 2024/05/06 13:58:28 by aglanuss         ###   ########.fr       */
+/*   Updated: 2024/05/06 14:33:57 by aglanuss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/philosophers.h"
 #include "../includes/utils.h"
+
+/**
+ * Initialize philos and forks variables.
+ * If any of variables fails, free both and return 0, else return 1.
+*/
+static int	init_variables(t_philo ***philos, pthread_mutex_t **forks, int num_of_philos)
+{
+	init_philos(philos, num_of_philos);
+	if (!*philos)
+		return (0);
+	init_forks(forks, num_of_philos);
+	if (!*forks)
+	{
+		free_philos(philos, num_of_philos);
+		return (0);
+	}
+	return (1);
+}
 
 int	main(int argc, char **argv)
 {
@@ -27,15 +45,8 @@ int	main(int argc, char **argv)
 	philos = NULL;
 	forks = NULL;
 	num_of_philos = ft_atoi(argv[1]);
-	init_philos(&philos, num_of_philos);
-	if (!philos)
+	if(!init_variables(&philos, &forks, num_of_philos))
 		return (EXIT_FAILURE);
-	init_forks(&forks, num_of_philos);
-	if (!forks)
-	{
-		free_philos(&philos, num_of_philos);
-		return (EXIT_FAILURE);
-	}
 	free_philos(&philos, num_of_philos);
 	free_forks(&forks, num_of_philos);
 	return (0);
