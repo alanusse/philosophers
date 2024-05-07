e2df9cf0a1c8f365adf757eb45b9e51db872997c
Agustín Lanusse <lanusseagustin@gmail.com>
2024-05-07 12:46:56 +0200

    added comments to philos functions

--- philo/src/philos.c
+++ philo/src/philos.c
@@ -6,12 +6,22 @@
 /*   By: aglanuss <aglanuss@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/03 10:58:29 by aglanuss          #+#    #+#             */
-/*   Updated: 2024/05/06 14:06:41 by aglanuss         ###   ########.fr       */
+/*   Updated: 2024/05/07 12:46:33 by aglanuss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/philosophers.h"
 
+/**
+ * Frees an array of philosophers, ensuring each philosopher's structure
+ * is freed and then the entire array itself.
+ * It sets each philosopher pointer to NULL after freeing to prevent dangling
+ * references. The function checks if the pointer to the array is not NULL
+ * before proceeding and iterates up to the specified number of philosophers.
+ *
+ * @param philos Pointer to the array of philosopher pointers to be freed.
+ * @param num_of_philos Number of philosopher pointers in the array.
+ */
 void  free_philos(t_philo ***philos, int num_of_philos)
 {
   int i;
@@ -29,11 +39,16 @@ void  free_philos(t_philo ***philos, int num_of_philos)
 }
 
 /**
- * Initialize philosophers.
- * Allocate an array containing each philosopher and each one has its own id.
- * If the allocation fails, iterate from the failed allocation to the start
- * freeing each philosopher.
-*/
+ * Initializes an array of pointers to t_philo structures, allocating memory
+ * for each philosopher and setting their IDs. If any allocation fails during
+ * the initialization, it frees all previously allocated memory to avoid
+ * memory leaks, using the free_philos function.
+ * Each philosopher pointer is initially set to NULL before memory allocation
+ * to ensure safe cleanup in case of failure.
+ *
+ * @param philos Pointer to the array of philosopher pointers to be initialized.
+ * @param num_of_philos Number of philosophers to initialize in the array.
+ */
 void  init_philos(t_philo ***philos, int num_of_philos)
 {
 	int i;
